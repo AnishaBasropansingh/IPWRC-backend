eef0e40da20b8e8ba8d735908defc4334b88a9d8
AzureAD\AnishaBasropansingh <abasropansingh@swis.nl>
2025-10-24 21:07:05 +0200

    fix cors errors for log in

--- webshop-backend/src/main/java/com/example/webshop_backend/controller/UserController.java
+++ webshop-backend/src/main/java/com/example/webshop_backend/controller/UserController.java
@@ -4,14 +4,12 @@ import com.example.webshop_backend.dao.UserDAO;
 import com.example.webshop_backend.model.CustomUser;
 import com.example.webshop_backend.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/user")
 class UserController {
     private final UserDAO userDAO;
